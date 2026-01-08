251612d379d5fff6bf8c6a9537b2b6539db6923b
zilongayan <zilongxelo@gmail.com>
2026-01-08 23:48:44 +0100

    Add application file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Hello " + name + "!!3"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
